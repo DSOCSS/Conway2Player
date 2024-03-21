076dff4298276e8a1f9953247878125008316018
ribhuhooja <ribhu.hooja.27@dartmouth.edu>
2024-03-21 15:44:03 -0400

    Added a function to check winning player; added 'lets' in front of variables that I had forgotten :P

--- conway.js
+++ conway.js
@@ -1,6 +1,10 @@
 // global assumptions:
 // there are two alive states, 'R' and 'B'
 
+let Constants = {
+  VICTORY_THRESHOLD: 0.8,
+};
+
 // **************** functions **************************** /
 
 // computes and returns the next next generation 
@@ -13,10 +17,10 @@
 // Implementation notes - any value other than 'R' or 'B' will
 // be considered dead
 function nextGeneration(prev, neighborIndexFn) {
-  maxI = prev.length;
-  maxJ = prev[0].length;
+  let maxI = prev.length;
+  let maxJ = prev[0].length;
   // make 2D array
-  result = Array.from({ length: maxI },
+  let result = Array.from({ length: maxI },
     () => Array(maxJ).fill(null)
   );
 
@@ -33,9 +37,9 @@ function nextGeneration(prev, neighborIndexFn) {
 // calculated using the previous generation and a given
 // next state function
 function nextState(i, j, maxI, maxJ, prev, neighborIndexFn) {
-  neighbors = neighborIndexFn(i, j, maxI, maxJ).map((i, j) => prev[i][j]);
-  numR = 0;
-  numB = 0;
+  let neighbors = neighborIndexFn(i, j, maxI, maxJ).map((i, j) => prev[i][j]);
+  let numR = 0;
+  let numB = 0;
   for (neighbor of neighbors) {
     if (neighbor === "R") {
       numR++;
@@ -44,7 +48,7 @@ function nextState(i, j, maxI, maxJ, prev, neighborIndexFn) {
     }
   }
 
-  numAlive = numR + numB;
+  let numAlive = numR + numB;
   if (numAlive < 2 || numAlive > 3) {
     return NULL;
   } else if (numAlive === 2) {
@@ -57,7 +61,7 @@ function nextState(i, j, maxI, maxJ, prev, neighborIndexFn) {
 // returns an array of all indices adjacent to the passed index 
 // borders are considered solid and so bordering cells have fewer neighbors
 function nowrapNeighborIndices(i, j, maxI, maxJ) {
-  result = new Array();
+  let result = new Array();
   for (x = Math.max(0, i - 1); x < Math.min(maxI, i + 2); x++) {
     for (y = Math.max(0, j - 1); y < Math.min(maxJ, j + 2); y++) {
       if (!(x == i && y == j)) {
@@ -71,7 +75,7 @@ function nowrapNeighborIndices(i, j, maxI, maxJ) {
 // borders wrap around i.e. the last row is adjacent to the first
 // and same for colums
 function wrapNeighborIndices(i, j, maxI, maxJ) {
-  result = new Array();
+  let result = new Array();
   for (x = (i - 1) % maxI; x <= (i + 1) % maxI; x++) {
     for (y = (j - 1) % maxJ; y <= (j + 1) % maxJ; y++) {
       if (!(x == i && y == j)) {
@@ -80,3 +84,49 @@ function wrapNeighborIndices(i, j, maxI, maxJ) {
     }
   }
 }
+
+// returns which player, if any, has won the game
+// if no player has won, returns null
+// TODO: maybe have an enum or something else
+//
+// Input - a 2d array of strings
+function winner(board) {
+  let reds = 0;
+  let blues = 0;
+  let other = 0;
+  for (i of board.flat()) {
+    if (i === "R") {
+      reds++;
+    } else if (i === "B") {
+      blues++;
+    } else {
+      other++;
+    }
+  }
+
+  let total = reds + blues + other;
+  let threshold = Constants.VICTORY_THRESHOLD * total;
+  if (reds > threshold) {
+    return "R";
+  }
+
+  if (blues > threshold) {
+    return "B"
+  }
+
+  return null;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
